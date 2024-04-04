e89bb3e50e36baa4356204ca9f65d9652e75cbc6
Fabian <27745262+TheLethalGoose@users.noreply.github.com>
2024-04-04 03:33:57 +0200

    Carousel pt optimisations

--- src/components/FloatingMenu.vue
+++ src/components/FloatingMenu.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="fixed top-0 left-0 right-0 w-screen flex mt-5 justify-center ">
     <Toolbar
-        class="border-primary w-full flex align-content-center"
-        style="margin-left: 20vw; margin-right: 20vw; height: 9vh"
+        class="border-primary w-full flex flex-nowrap"
+        style="margin-left: 18vw; margin-right: 18vw; height: 8vh; min-height:4rem"
     >
       <template #start>
         <TerrainPick/>
       </template>
       <template #center>
-        <TileCarousel/>
+          <TileCarousel/>
       </template>
       <template #end>
         <Button disabled icon="pi pi-print" class="mr-2" severity="secondary" />
--- src/components/TileCarousel.vue
+++ src/components/TileCarousel.vue
@@ -1,13 +1,22 @@
 <template>
-  <Carousel :value="tiles" :numVisible="1" :numScroll="1" :show-indicators="false" container-class="align-items-center">
-    <template #item="slotProps">
-
-    </template>
-  </Carousel>
+    <Carousel
+        pt:item:style="width: 1px"
+        pt:previousButton:class="mr-6"
+        circular
+        :value="tiles"
+        :numVisible="4"
+        :numScroll="1"
+        :show-indicators="false"
+        container-class="align-items-center justify-items-center">
+      <template #item="slotProps">
+          <TileDraggable/>
+      </template>
+    </Carousel>
 </template>
 
 <script setup lang="ts">
     import {ref} from "vue";
     import Tile from "@/components/Tile.vue";
-    const tiles = ref([]);
+    import TileDraggable from "@/components/TileDraggable.vue";
+    const tiles = ref([1,2,3,4,5,6,7,8,9,10]);
 </script>
