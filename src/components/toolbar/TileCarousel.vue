26adc1aa874ba4e40e42fbbb6fd68a572bd04434
Fabian <27745262+TheLethalGoose@users.noreply.github.com>
2024-04-06 00:14:52 +0200

    Align carousel items centered

--- src/components/toolbar/TileCarousel.vue
+++ src/components/toolbar/TileCarousel.vue
@@ -2,10 +2,13 @@
     <Carousel
         circular
         :value="tiles"
+        pt:item:class="align-self-center"
+        pt:itemCloned:class="align-self-center"
+        container-class="align-items-center"
         :numVisible="5"
         :numScroll="1"
         :show-indicators="false"
-        class="w-18rem"
+        class="w-19rem"
         >
         <template #item="slotProps">
           <TileDraggable :hexes="slotProps.data"/>
@@ -15,8 +18,7 @@
 
 <script setup lang="ts">
     import {ref} from "vue";
-    import Tile from "@/components/dragAndDrop/Tile.vue";
     import TileDraggable from "@/components/dragAndDrop/TileDraggable.vue";
-    import GrassTile from "@/assets/terrain/grass.svg";
     const tiles = ref([1,2,3,7,24]);
+
 </script>
