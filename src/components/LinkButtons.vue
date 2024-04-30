6ce01d3731126d4a3cf6f130135840fabe0704f5
Fabian Teufel <27745262+TheLethalGoose@users.noreply.github.com>
2024-04-30 11:46:35 +0200

    Repo link on button

--- src/components/LinkButtons.vue
+++ src/components/LinkButtons.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="fixed bottom-0 right-0 flex m-3 justify-center">
-    <Button icon="pi pi-github" severity="contrast" outlined/>
+    <Button
+        icon="pi pi-github"
+        severity="contrast"
+        outlined
+        @click="openLink('https://github.com/TheLethalGoose/HeroscapeMapBuilder')"
+    />
   </div>
 </template>
+
+<script setup lang="ts">
+
+function openLink(link: string) {
+  window.open(link, '_blank');
+}
+
+</script>
